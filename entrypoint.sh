50ce133bd0c900652b1d1ccbbfb7225df3649e63
blaufurenos <81584208+blaufurenos@users.noreply.github.com>
2022-10-05 08:16:05 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ cat << EOF > /etc/xray/config.json
 {
   "inbounds": [
     {
-      "port": $PORT,
+      "port": 8080,
       "protocol": "vless",
       "settings": {
         "decryption": "none",
